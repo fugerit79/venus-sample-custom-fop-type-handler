7e00b6ead8ad3b36faa158da2a52b3dccff79a51
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-09-29 22:48:44 +0200

    feat(config): project initialization
    mvn org.fugerit.java:fj-doc-maven-plugin:8.16.5:init \
    -DgroupId=org.fugerit.java.demo \
    -DartifactId=venus-sample-custom-fop-type-handler \
    -Dextensions=base,freemarker,mod-fop \
    -Dflavour=quarkus-3

---
+++ src/main/java/org/fugerit/java/demo/venussamplecustomfoptypehandler/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.fugerit.java.demo.venussamplecustomfoptypehandler;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/test/org/fugerit/java/demo/venussamplecustomfoptypehandler/DocResourceIT.java
@@ -0,0 +1,8 @@
+package test.org.fugerit.java.demo.venussamplecustomfoptypehandler;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class DocResourceIT extends DocResourceTest {
+    // Execute the same tests but in packaged mode.
+}
